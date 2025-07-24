a71229532e0bdaa9114dab8bca8f7ffa6bae95c2
root <root@LAPTOP-FOBQ7CEC.>
2025-07-25 00:31:11 +0100

    Add main file

---
+++ train.c
@@ -0,0 +1,19 @@
+#include "train.h"
+
+
+int main(){
+    printf("-------------------------------------------------------------------------INICIO-------------------------------------------------------------------------\n");
+    seat_node *headofseats=create_list_of_seats();
+    train_node *headlistoftrains=create_list_of_trains("Pombal","Coimbra");
+    station_node *headlistofstations = create_list_of_stations("Pombal");
+    addstation(headlistofstations,"Coimbra");
+    addtrain(headlistoftrains,"Coimbra","Pombal");
+    print_current_time();
+    print_interface_options(headlistofstations,headlistoftrains,headofseats);
+    reserve_seat(headofseats,"1","RUI");
+    reserve_seat(headofseats,"13","matilde");
+    
+    
+    printf("\n\n-------------------------------------------------------------------------FIM----------------------------------------------------------------------------\n");
+    return 0;
+}
